057b3322d3fb2939d14abfdb71f65df942bba9e5
chkyass <chkyass@example.com>
2020-02-29 19:36:20 +0100

    add method to fetch last 50 messages

--- src/main/java/chkyass/repository/MessageRepository.java
+++ src/main/java/chkyass/repository/MessageRepository.java
@@ -2,9 +2,6 @@ package chkyass.repository;
 
 import chkyass.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,9 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    @Modifying
-    @Query("delete from Message m where m.user =:user")
-    void deleteUserMessages(@Param("user") String user);
+    Optional<List<Message>> findFirst50ByRoomIdOrderByTimestampDesc(long roomId);
 
-    Optional<List<Message>> findFirst20ByOrderByTimestampDesc();
 }
