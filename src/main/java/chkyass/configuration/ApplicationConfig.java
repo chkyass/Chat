1d51bd51c84b871f7661f4681a735c542b6f1e7c
chkyass <chkyass@example.com>
2020-02-29 18:36:34 +0100

    add JavaMailSender Bean

--- src/main/java/chkyass/configuration/ApplicationConfig.java
+++ src/main/java/chkyass/configuration/ApplicationConfig.java
@@ -1,14 +1,46 @@
 package chkyass.configuration;
 
+import chkyass.ChatApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+import java.util.Properties;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
     public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
         return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
+
+    @Autowired
+    private Environment env;
+
+    Logger logger = LoggerFactory.getLogger(ChatApplication.class);
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        logger.info("gmail.account: " + env.getProperty("gmail.account"));
+        mailSender.setUsername(env.getProperty("gmail.account"));
+        mailSender.setPassword(System.getenv("GMAIL_PASS"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "false");
+
+        return mailSender;
     }
 }
